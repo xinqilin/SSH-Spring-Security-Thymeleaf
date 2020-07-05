1b96180bdbff23a3a326ab2a6a12fcefa15834ae
BillLin1209 <zzx123bill@gmail.com>
2020-07-05 19:36:29 +0800

    first to ssh framework

---
+++ src/main/java/com/bill/sshAndSecurity/SshAndSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.bill.sshAndSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshAndSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshAndSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bill/sshAndSecurity/SshAndSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.sshAndSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshAndSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
