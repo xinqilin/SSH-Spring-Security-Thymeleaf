aefc8b857af3c22b8e9301e48fb995295727cca2
BillLin1209 <zzx123bill@gmail.com>
2020-07-05 20:12:19 +0800

    table schema generate by jpa

--- src/main/java/com/bill/sshAndSecurity/model/Role.java
+++ src/main/java/com/bill/sshAndSecurity/model/Role.java
@@ -1,5 +1,21 @@
 package com.bill.sshAndSecurity.model;
 
-public class Role {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
 }
